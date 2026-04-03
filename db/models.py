dce0d7691a59572e88b30775aba86f975693bc4d
Adonai <adonaisoares694@gmail.com>
2026-04-03 17:26:45 -0300

    Implement movies 3

--- db/models.py
+++ db/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    actors = models.ManyToManyField("Actor", related_name="movies")
-    genres = models.ManyToManyField("Genre", related_name="movies")
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="movies")
 
     def __str__(self) -> str:
         return self.title
@@ -23,8 +38,6 @@ class CinemaHall(models.Model):
     def __str__(self) -> str:
         return self.name
 
-
-# ... (restante do código acima permanece igual)
 
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
